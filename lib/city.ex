ac639adb37b3995265be0f019974148198f05dda
Arnauld <arnauld.loyer@gmail.com>
2016-01-01 01:52:55 +0100

    add notification

--- lib/city.ex
+++ lib/city.ex
@@ -22,12 +22,22 @@ defmodule City do
     end)
   end
 
-  def increase_infection_level(city, disease) do 
+  def increase_infection_level(city, disease, originator \\ :nil) do 
+    ref = :erlang.make_ref()
     Agent.update(city, fn {name, links, levels} ->
       level = Map.get(levels, disease, 0)
-      new_levels = Map.put(levels, disease, level + 1)
-      {name, links, new_levels}
+      case level do
+        _ when level >= 3 ->
+          send_if_not_nil originator, {:infection_level_increased, city, ref, disease, :outbreak}
+          {name, links, levels}
+        _ ->
+          new_level = level + 1
+          new_levels = Map.put(levels, disease, new_level)
+          send_if_not_nil originator, {:infection_level_increased, city, ref, disease, new_level}
+          {name, links, new_levels}
+      end
     end)
+    {:ok, ref}
   end
 
   def change_infection_level(city, disease, new_level) do
@@ -35,5 +45,14 @@ defmodule City do
       new_levels = Map.put(levels, disease, new_level)
       {name, links, new_levels}
     end)
+  end
+
+  defp send_if_not_nil(pid, msg) do
+    case pid do
+      :nil ->
+        :ok
+      _    ->
+        send pid, msg
+    end
   end
 end
--- test/city_test.exs
+++ test/city_test.exs
@@ -3,12 +3,12 @@ defmodule CityTest do
   doctest City
 
   setup do
-  	{:ok, pid} = City.start_link(:london)
-  	on_exit fn ->
-  		# We don’t need to explictly shut down the Agent
-  		# because it will receive a :shutdown signal 
-  		# when our test finishes. 
-  		:agent_automatically_stopped
+      {:ok, pid} = City.start_link(:london)
+      on_exit fn ->
+          # We don’t need to explictly shut down the Agent
+          # because it will receive a :shutdown signal 
+          # when our test finishes. 
+          :agent_automatically_stopped
         #City.stop(pid)
     end
     {:ok, pid: pid}
@@ -23,8 +23,39 @@ defmodule CityTest do
     assert [{:blue, 1}] == City.infection_levels(:london)
   end
 
+  test "increase infection level which then trigger an async notification" do
+    {:ok, ref} = City.increase_infection_level(:london, :blue, self())
+    receive do
+        {:infection_level_increased, city, ref0, disease, new_level} ->
+            assert 1       == new_level
+            assert :blue   == disease
+            assert :london == city
+            assert ref     == ref0
+    end
+  end
+
   test "change infection level" do
-  	City.change_infection_level(:london, :pink, 4)
+      City.change_infection_level(:london, :pink, 4)
     assert [{:pink, 4}] == City.infection_levels(:london)
+  end
+
+  test "cannot increase infection level above 3" do
+    City.change_infection_level(:london, :pink, 3)
+    City.increase_infection_level(:london, :pink)
+    assert [{:pink, 3}] == City.infection_levels(:london)
+  end
+
+  test "cannot increase infection level above 3 which then trigger an outbreak notification" do
+    City.change_infection_level(:london, :green, 3)
+    {:ok, ref} = City.increase_infection_level(:london, :green, self())
+    receive do
+        {:infection_level_increased, city, ref0, disease, :outbreak} ->
+            assert :green  == disease
+            assert :london == city
+            assert ref     == ref0
+    after
+       1_000 -> 
+               assert false, "Timeout"
+    end
   end
 end
