24bc125af06caf0aad8282b0c0bce568eb3e650f
Arnauld <arnauld.loyer@gmail.com>
2016-01-02 18:20:44 +0100

    reformat

--- test/infestor_test.exs
+++ test/infestor_test.exs
@@ -8,12 +8,12 @@ defmodule InfestorTest do
                   {:paris,  [:london, :madrid]}]
     CitySup.start_link(city_specs)
 
-  City.change_infection_level(:london, :blue, 2)
-  City.change_infection_level(:madrid, :blue, 1)
+    City.change_infection_level(:london, :blue, 2)
+    City.change_infection_level(:madrid, :blue, 1)
 
-  {:ok, journal} = Infestor.infect(:london, :blue)
-  assert [{:propagate, :london}, 
-          {:infected, :london, 3}] == journal
+    {:ok, journal} = Infestor.infect(:london, :blue)
+    assert [{:propagate, :london}, 
+            {:infected, :london, 3}] == journal
 
     assert [{:blue, 3}] == City.infection_levels(:london)
     assert [{:blue, 1}] == City.infection_levels(:madrid)
@@ -26,16 +26,16 @@ defmodule InfestorTest do
             {:paris,  [:london, :madrid]}]
     CitySup.start_link(city_specs)
 
-  City.change_infection_level(:london, :blue, 3)
-  City.change_infection_level(:madrid, :blue, 1)
+    City.change_infection_level(:london, :blue, 3)
+    City.change_infection_level(:madrid, :blue, 1)
 
-  {:ok, journal} = Infestor.infect(:london, :blue)
-  assert [{:propagate, :london}, 
-          {:outbreak, :london}, 
-          {:propagate, :paris}, 
-          {:propagate, :madrid}, 
-          {:infected, :paris, 1},
-          {:infected, :madrid, 2}] == journal
+    {:ok, journal} = Infestor.infect(:london, :blue)
+    assert [{:propagate, :london}, 
+            {:outbreak, :london}, 
+            {:propagate, :paris}, 
+            {:propagate, :madrid}, 
+            {:infected, :paris, 1},
+            {:infected, :madrid, 2}] == journal
 
     assert [{:blue, 3}] == City.infection_levels(:london)
     assert [{:blue, 2}] == City.infection_levels(:madrid)
@@ -48,23 +48,22 @@ defmodule InfestorTest do
                   {:paris,  [:london, :madrid]}]
     CitySup.start_link(city_specs)
 
-  City.change_infection_level(:london, :blue, 3)
-  City.change_infection_level(:madrid, :blue, 3)
-  City.change_infection_level(:paris,  :blue, 2)
+    City.change_infection_level(:london, :blue, 3)
+    City.change_infection_level(:madrid, :blue, 3)
+    City.change_infection_level(:paris,  :blue, 2)
 
-  {:ok, journal} = Infestor.infect(:london, :blue)
-  assert [{:propagate, :london}, 
-          {:outbreak, :london}, 
-          {:propagate, :paris}, 
-          {:propagate, :madrid}, 
-          {:infected, :paris, 3},
-          {:outbreak, :madrid}, 
-          {:propagate, :paris}, 
-          {:outbreak, :paris}] == journal
+    {:ok, journal} = Infestor.infect(:london, :blue)
+    assert [{:propagate, :london}, 
+            {:outbreak, :london}, 
+            {:propagate, :paris}, 
+            {:propagate, :madrid}, 
+            {:infected, :paris, 3},
+            {:outbreak, :madrid}, 
+            {:propagate, :paris}, 
+            {:outbreak, :paris}] == journal
 
     assert [{:blue, 3}] == City.infection_levels(:london)
     assert [{:blue, 3}] == City.infection_levels(:madrid)
     assert [{:blue, 3}] == City.infection_levels(:paris)
-
   end
 end
